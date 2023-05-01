dd6ff283dd860680925402ec0965024131264950
daniladolzhikov <d.dolzhikov17@gmail.com>
2023-05-01 12:12:24 +0200

    initial commit

---
+++ task23/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход 
+// число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+void Cube(int N) 
+{
+    int count = 1;
+    while (count <= N)
+    {
+        Console.WriteLine(count + " - " +count*count*count);
+        count++;
+    }
+}
+
+Console.Write("Введите N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+Cube(N);
